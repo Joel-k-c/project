862ff9b246e7866025a8c60abba815798154dab4
Joel-k-c <76583252+Joel-k-c@users.noreply.github.com>
2021-05-17 20:27:11 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,12 @@
+function red_btn(){
+    document.body.style.background="red";
+
+}
+function yellow_btn(){
+    document.body.style.background="yellow";
+
+}
+function green_btn(){
+    document.body.style.background="green";
+
+}
